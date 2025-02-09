14be99790974db9cf13aef6dc4fe1b2870c55860
Leonardo Arellano Martinez <leo.aremtz98@gmail.com>
2025-02-08 23:54:15 -0600

    Import codebuild project and add buildspec file

--- terraform/cicd-pipeline/codebuild.tf
+++ terraform/cicd-pipeline/codebuild.tf
@@ -35,3 +35,39 @@ resource "aws_iam_role_policy_attachment" "codebuild_role_cloudwatch_logs_policy
   policy_arn = aws_iam_policy.cloudwatch_logs.arn
   role       = aws_iam_role.codebuild_service_role.name
 }
+
+################################################################################
+# Build Project para la web app de Vue
+################################################################################
+resource "aws_codebuild_project" "build_vue_project" {
+  name         = local.aws_codebuild_vue_project_name
+  description  = "Compila la aplicación de VueJS"
+  service_role = aws_iam_role.codebuild_service_role.arn
+
+  build_timeout = 7
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type                        = "LINUX_LAMBDA_CONTAINER"
+    compute_type                = "BUILD_LAMBDA_1GB"
+    image                       = "aws/codebuild/amazonlinux-x86_64-lambda-standard:nodejs18"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file(abspath("./buildspecs/vue_buildspec.yml"))
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = local.aws_codebuild_project_group_log_name
+      status     = "ENABLED"
+    }
+  }
+
+  tags = merge(local.merged_tags, {})
+}
